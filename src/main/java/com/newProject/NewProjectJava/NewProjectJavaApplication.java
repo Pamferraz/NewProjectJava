f353b510c8aa756cc007add4df0148861f310e9e
JulioFreitas <juliosparapan2014@gmail.com>
2025-04-09 22:13:10 -0300

    primeito commit

---
+++ src/main/java/com/newProject/NewProjectJava/NewProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.newProject.NewProjectJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewProjectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewProjectJavaApplication.class, args);
+	}
+
+}
